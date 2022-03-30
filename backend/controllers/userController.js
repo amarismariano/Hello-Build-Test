272e0082d19ced42d0247ff50a8d9a193bd2339b
amarismariano <mariano32josea@gmail.com>
2022-03-30 18:38:22 -0500

    Enviando primeras request, almacenando en db

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,13 @@
-const register = (req, res) => {};
+import User from "../models/User.js";
+
+const register = async (req, res) => {
+  try {
+    const user = new User(req.body);
+    const savedUser = await user.save();
+    res.json(savedUser);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export { register };
--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
+app.use(express.json());
 
 dotenv.config();
 
