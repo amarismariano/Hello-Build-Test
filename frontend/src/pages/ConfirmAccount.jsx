d77b086795f8100d2e5faec1183f338b08085512
amarismariano <mariano32josea@gmail.com>
2022-03-30 22:41:44 -0500

    Rest of the pages

--- frontend/src/pages/ConfirmAccount.jsx
+++ frontend/src/pages/ConfirmAccount.jsx
@@ -1,5 +1,12 @@
 const ConfirmAccount = () => {
-  return <div>ConfirmAccount</div>;
+  return (
+    <>
+      <h1 className="text-amber-600 font-black text-5xl capitalize">
+        Confirm your account and start searching{" "}
+        <span className="text-slate-700">Repositories!</span>
+      </h1>
+    </>
+  );
 };
 
 export default ConfirmAccount;
--- frontend/src/pages/NewPassword.jsx
+++ frontend/src/pages/NewPassword.jsx
@@ -1,5 +1,34 @@
 const NewPassword = () => {
-  return <div>NewPassword</div>;
+  return (
+    <>
+      <h1 className="text-amber-600 font-black text-5xl capitalize">
+        Reset your <span className="text-slate-700">Password!</span>
+      </h1>
+
+      <form className="my-10 bg-white shadow rounded-lg p-10">
+        <div className="my-5">
+          <label
+            htmlFor="password"
+            className="uppercase text-gray-600 block text-xl font-bold"
+          >
+            New Password
+          </label>
+          <input
+            id="password"
+            type="password"
+            placeholder="New Password"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          />
+        </div>
+
+        <input
+          type="submit"
+          value="Save New Password"
+          className="bg-amber-700 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-amber-800 transition-colors"
+        />
+      </form>
+    </>
+  );
 };
 
 export default NewPassword;
