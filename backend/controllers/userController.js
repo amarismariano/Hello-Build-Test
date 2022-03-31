e16f256f4bbac136adbbae3befa056d72c55802d
amarismariano <mariano32josea@gmail.com>
2022-03-30 19:24:47 -0500

    Adding the JWT for further validations

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 import generateId from "../helpers/idGenerator.js";
+import generateJWT from "../helpers/JWTGenerator.js";
 
 const register = async (req, res) => {
   // Not allowing duplicate users
@@ -22,6 +23,34 @@ const register = async (req, res) => {
   }
 };
 
-const authentication = async (req, res) => {};
+const authentication = async (req, res) => {
+  const { email, password } = req.body;
+
+  //Check if the user exist
+  const user = await User.findOne({ email });
+  if (!user) {
+    const error = new Error("This user does not exist");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  //Check if the user is confirmed
+  if (!user.confirmed) {
+    const error = new Error("Your account has not been confirmed");
+    return res.status(403).json({ msg: error.message });
+  }
+
+  // Check the pw of the user
+  if (await user.authPassword(password)) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateJWT(user._id),
+    });
+  } else {
+    const error = new Error("Password is Incorrect");
+    return res.status(403).json({ msg: error.message });
+  }
+};
 
 export { register, authentication };
--- backend/models/User.js
+++ backend/models/User.js
@@ -40,5 +40,9 @@ userSchema.pre("save", async function (next) {
   this.password = await bcrypt.hash(this.password, salt);
 });
 
+userSchema.methods.authPassword = async function (passwordForm) {
+  return await bcrypt.compare(passwordForm, this.password);
+};
+
 const User = mongoose.model("User", userSchema);
 export default User;
