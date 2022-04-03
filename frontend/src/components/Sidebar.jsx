a49423391eed454edd71cc116205f8071446e4f2
amarismariano <mariano32josea@gmail.com>
2022-04-03 12:49:10 -0500

    Css

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -12,9 +12,11 @@ const Sidebar = () => {
 
   return (
     <aside className="md:w-80 lg:w-96 px-5 py-10 border">
-      <p className="text-xl font-bold">Hey! {auth.name}</p>
+      <p className="text-xl font-bold">
+        Hey! <span className="text-amber-600 uppercase">{auth.name}</span>
+      </p>
 
-      <p className="text-md capitalize">Start searching:</p>
+      <p className="text-md capitalize font-bold">Start searching:</p>
       <input
         className=" w-full p-3  font-bold block mt-5  text-center rounded-lg "
         placeholder="Search for a User"
